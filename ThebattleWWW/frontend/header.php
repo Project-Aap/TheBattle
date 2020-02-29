555b2c7697ee9e7eb83a756fbedde6843b13549a
milanvanpelt2003 <milanvanpelt2003@gmail.com>
2020-02-29 17:03:58 +0100

    styled and finished the login form

--- ThebattleWWW/frontend/header.php
+++ ThebattleWWW/frontend/header.php
@@ -7,6 +7,7 @@
   <title>Curio.nl | TheBattle</title>
   <link rel="stylesheet" href="style.css">
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+  <link rel="stylesheet" href="css/login_style.css">
 </head>
 
 <body>
@@ -22,7 +23,17 @@
           <a href="#">TheBattle</a>
           <a href="#">Contact </a>
           <!-- if you want to add forms for the login system i would put it in here -->
-          <a href="login.php">Login</a>
+          <?php
+            if (isset($_SESSION["idUsers"])) {
+              echo '<a href="AdminPanel/sendMails.php">AdminPanel</a>';
+                  echo '<form class="logout" action="../backend/includes/logout.inc.php" method="POST">';
+                      echo '<input type="submit" name="logout-submit" value="logout">';
+                  echo '</form>';
+              } else {
+                  // ============Dit krijg je te zien als je niet bent ingelogged=========
+                  echo '<a href="login.php">Login</a>';
+              }
+            ?>
           <!-- =========================================================== -->
         </div>
         <a href="javascript:void(0);" class="icon" onclick="myFunction()">
--- ThebattleWWW/frontend/login.php
+++ ThebattleWWW/frontend/login.php
@@ -2,27 +2,22 @@
   session_start();
 ?>
 <?php require("header.php");?>
+<body class="login">
     <main>
         <?php
             if (isset($_SESSION["idUsers"])) {
-                echo '<form action="../backend/includes/logout.inc.php" method="POST">';
-                    echo '<input type="submit" name="logout-submit" value="logout">';
-                echo '</form>';
-                // =====================Hier kan je iets zetten als je ingelogged bent==============================
-                echo '<p>You are logged in!</p>';
-                // =====================Dit kan je wegdoen maar je kan bijvoorbeeld ook de naam uit de database halen=============
-                echo '<p>User:' . $_SESSION["idUsers"] . '</p>';
+                echo '<p class="login-text">Welkom user '  . $_SESSION["idUsers"] . '!</p>';
             } else {
                 // ============Dit krijg je te zien als je niet bent ingelogged=========
-                echo '<form action="../backend/includes/login.inc.php" method="POST">';
+                echo '<form class="login-flex flex-reverse align-center" action="../backend/includes/login.inc.php" method="POST">';
                     if (isset($_GET["uid"])) {
                         $uid = $_GET["uid"];
-                        echo '<input type="text" name="uid" placeholder="Username" value="' . $uid . '">';
+                        echo '<input class="login-input" type="text" name="uid" placeholder="Username" value="' . $uid . '">';
                     } else {
-                        echo '<input type="text" name="uid" placeholder="Username">';
+                        echo '<input class="login-input" type="text" name="uid" placeholder="Username">';
                     }
-                    echo '<input type="password" name="pwd" placeholder="Password">';
-                    echo '<input type="submit" name="login-submit" value="login">';
+                    echo '<input class="login-input" type="password" name="pwd" placeholder="Password">';
+                    echo '<input class="login-submit" type="submit" name="login-submit" value="login">';
                 echo '</form>';
             }
 
