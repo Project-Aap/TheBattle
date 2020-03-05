e57aa405bcf8d976f30b1a22db982530cb73c54e
Satou-san <31078311+Slimestone@users.noreply.github.com>
2020-03-05 09:51:13 +0100

    functioneert

--- ThebattleWWW/frontend/header.php
+++ ThebattleWWW/frontend/header.php
@@ -43,6 +43,16 @@
     <i class="fa fa-bars"></i>
   </a>
 </div>
+<script>
+      function myFunction() {
+  var x = document.getElementById("myLinks");
+  if (x.style.display === "block") {
+    x.style.display = "none";
+  } else {
+    x.style.display = "block";
+  }
+} 
+    </script>
 
     <!-- =================================================== -->
   </header>
