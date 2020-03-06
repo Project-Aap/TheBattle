38620a26b800dd278d9729c4629dac0925b1e110
Morris420 <morrissmit2312@gmail.com>
2020-03-06 15:21:21 +0100

    Morris Commit kaas

--- admin-panel/admin-article/classes/Dbh.class.php
+++ admin-panel/admin-article/classes/Dbh.class.php
@@ -1,10 +1,9 @@
 <?php
     class Dbh {
-        private $host = "localhost";
-        private $dbname = "admin-articles";
-        private $uid = "root";
-        private $pwd = "";
-
+        private $host = "sql213.epizy.com";
+        private $dbname = "epiz_25235384_articledb";
+        private $uid = "epiz_25235384";
+        private $pwd = "EuJCv1EE366U0X";
         protected function connect() {
             $dsn = "mysql:host=" . $this->host . ";dbname=" . $this->dbname . ";";
             $pdo = new PDO($dsn, $this->uid, $this->pwd);
