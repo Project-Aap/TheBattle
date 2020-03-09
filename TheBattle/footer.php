8f0bdf1a874bb831399fe3741cd29d18a02347d4
Jethrootje <36591615+Jethrootje@users.noreply.github.com>
2020-03-09 16:13:14 +0100

    Fixed Login & Footer

---
+++ TheBattle/footer.php
@@ -0,0 +1,53 @@
+	<footer>
+		<div class="container">
+			<div class="contact-form">
+				<h2>Contact</h2>
+				<h3>Infopunt@curio.nl</h3>
+			</div>
+			<div class="contact-links">
+					<div>
+						<h2>VMBO</h2>
+						<p>Text</p>
+						<p>texttt</p>
+						<p>text</p>						
+					</div>
+					<div>
+						<h2>MBO</h2>
+						<p>Text</p>
+						<p>texttt</p>
+						<p>text</p>
+					</div>
+					<div>
+						<h2>overige opleiding</h2>
+						<p>Text</p>
+						<p>texttt</p>
+						<p>text</p>
+					</div>
+					<div>
+						<h2>over curio</h2>
+						<p>Text</p>
+						<p>texttt</p>
+						<p>text</p>
+					</div>
+					<div>
+						<h2>hulp nodig?</h2>
+						<p>Text</p>
+						<p>texttt</p>
+						<p>text</p>
+					</div>
+					<div class="social-media">
+						<h2>curio</h2>
+						<p></p>
+						<img src="https://picsum.photos/40/40" alt="">
+						<img src="https://picsum.photos/40/40" alt="">
+						<img src="https://picsum.photos/40/40" alt="">
+						<img src="https://picsum.photos/40/40" alt="">							
+					</div>
+			</div>
+
+		</div>
+
+
+	</footer>
+</body>
+</html>
