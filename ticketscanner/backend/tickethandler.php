96375cc9e38af733d839f4a6f79508b05ddd1154
ThijmenDekker <thijmen@visioen.nl>
2020-03-09 15:15:54 +0100

    add ticket scanner

---
+++ ticketscanner/backend/tickethandler.php
@@ -0,0 +1,63 @@
+<?php
+
+$dbHost = 'sql213.epizy.com';
+$dbName = 'epiz_25235384_information';
+$dbUser = 'epiz_25235384';
+$dbPass = 'EuJCv1EE366U0X';
+
+$options = [
+    PDO::ATTR_ERRMODE            => PDO::ERRMODE_EXCEPTION,
+    PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+    PDO::ATTR_EMULATE_PREPARES   => false,
+];
+$dbConn = new PDO("mysql:host=$dbHost;dbname=$dbName;", $dbUser, $dbPass, $options);
+
+$number = $_POST['text'];
+
+$sql = "SELECT * FROM user_information WHERE id = :id";
+
+//prepare the query
+$prepare = $dbConn->prepare($sql);
+//change the placeholders for the correct value
+$prepare->execute([
+    ':id' => $number
+]);
+
+
+session_start();
+
+//fetch
+$item = $prepare->fetch(PDO::FETCH_ASSOC);
+
+
+//if the item is correctly recieved
+if ($item && $item["scanned"] == 0) {
+    //set the value to true
+    $_SESSION["checkAccess"] = 3;
+    $_SESSION["personName"] = $item['name'];
+
+    $sqledit = "UPDATE user_information SET scanned=1 WHERE id=:id";
+
+    $query = $dbConn->prepare($sqledit);
+    $query->execute([
+        ':id'                   => $number
+    ]);
+
+    //relocate to main page
+    header('Location: ' . '../ticket_scanner_main.php');
+    //dont activate other code
+    exit;
+}
+if ($item["scanned"] == 1) {
+    $_SESSION["checkAccess"] = 2;
+    header('Location: ' . '../ticket_scanner_main.php');
+    exit;
+}
+//set it to false (if it should be true, it wont be activated because of the exit)
+$_SESSION["checkAccess"] = 0;
+header('Location: ' . '../ticket_scanner_main.php');
+exit;
+
+
+
+?>
---
+++ ticketscanner/ticket_scanner_main.php
@@ -0,0 +1,59 @@
+<?php session_start(); ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Ticket Scanner pagina</title>
+	<link rel="stylesheet" href="css/ticket_style.css">
+</head>
+<body>
+    <div class="wrapper">
+        <?php
+            if (isset($_SESSION["validScanner"])) {
+                if($_SESSION["validScanner"]) {
+
+                } 
+                else {
+
+                    exit;
+                }
+            } else {
+
+                //exit;
+            }
+            if(isset($_SESSION["checkAccess"]))
+            {
+                if ($_SESSION["checkAccess"] == 2)
+                {
+                    echo '<div class="redBackground background">';
+                    echo "<h1>Heeft al ingecheckt.</h1>";
+                    unset($_SESSION["checkAccess"]);
+                }
+                else if($_SESSION["checkAccess"] == 3)
+                {
+                    echo '<div class="greenBackground background">';
+                    echo "<h1>". $_SESSION['personName'] . " heeft aangemeld.</h1>";
+                    unset($_SESSION["checkAccess"]);
+                }
+                else if ($_SESSION["checkAccess"] == 0)
+                {
+                    echo '<div class="redBackground background">';
+                    echo "<h1>Niet aangemeld.</h1>";
+                    unset($_SESSION["checkAccess"]);
+                }
+            } else {
+                echo '<div class="normalBackground background">';
+                echo "<h1>Welkom op de ticket scanner pagina.</h1>";
+            }
+        ?>
+            <form action="backend/tickethandler.php" method="POST">
+                <div>
+                    <input name="text" type="text" id="text" value="" autofocus required placeholder="Zorg dat je dit geselecteerd hebt">
+                </div>
+            </form>
+            <img src="img/background_circle.png" alt="background" id="background-image">
+        </div>
+    </div>
+</body>
+</html>
