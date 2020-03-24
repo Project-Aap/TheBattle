8b313df18ab1c0e9fe483d7ca202914477c00a36
milanvanpelt2003 <milanvanpelt2003@gmail.com>
2020-03-24 15:27:53 +0100

    added a local dbh in comments

--- adminPanel/articles/classes/Dbh.class.php
+++ adminPanel/articles/classes/Dbh.class.php
@@ -1,9 +1,14 @@
 <?php
     class Dbh {
-        private $host = "sql213.epizy.com";
-        private $dbname = "epiz_25235384_debattleDB";
-        private $uid = "epiz_25235384";
-        private $pwd = "EuJCv1EE366U0X";
+        // private $host = "sql213.epizy.com";
+        // private $dbname = "epiz_25235384_debattleDB";
+        // private $uid = "epiz_25235384";
+        // private $pwd = "EuJCv1EE366U0X";
+
+        private $host = "localhost";
+        private $dbname = "thebattle";
+        private $uid = "root";
+        private $pwd = "";
 
         protected function connect() {
             $dsn = "mysql:host=" . $this->host . ";dbname=" . $this->dbname . ";";
