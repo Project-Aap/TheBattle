8ef82f6d0de7d31d3bf40b9931f29c32eace7d2e
milanvanpelt2003 <milanvanpelt2003@gmail.com>
2020-03-26 09:54:11 +0100

    changed title adminPanel articles

--- adminPanel/articles/header.php
+++ adminPanel/articles/header.php
@@ -11,7 +11,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta charset="UTF-8">
     <!--=========================[Title Tags]=========================-->
-    <title>default-folder-structure</title>
+    <title>adminPanel articles</title>
     <!--=========================[Link Tags]=========================-->
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
     <link rel="stylesheet" href="styles/main.css">
